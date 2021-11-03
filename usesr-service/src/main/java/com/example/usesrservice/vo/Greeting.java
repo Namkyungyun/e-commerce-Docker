76858dcfea720aef52f9e6323e954b28b547c190
Yun Namkyung <nagy123@naver.com>
2021-11-04 01:05:23 +0900

    user-service

---
+++ usesr-service/src/main/java/com/example/usesrservice/vo/Greeting.java
@@ -0,0 +1,15 @@
+package com.example.usesrservice.vo;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component  // service, controller 등의 용도가 불분명한 상태에서 bean으로 등록하고자 할 때 사용
+@Data       // Lombok에 내장된 기능으로, getter-setter를 구현하지 않아도 됨.
+public class Greeting {
+
+    @Value("${greeting.message}")
+    private String message;
+
+
+}
---
+++ usesr-service/src/test/java/com/example/usesrservice/UsesrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usesrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsesrServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
